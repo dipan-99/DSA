14af59731ea01343d13aaed127e77745c63ca4d9
Dipan <dipansarkar76@gmail.com>
2026-07-16 22:45:16 +0530

    Implement pivot index calculation in LC724_Find_Pivot_Index class

--- Arrays/LC724_Find_Pivot_Index.java
+++ Arrays/LC724_Find_Pivot_Index.java
@@ -1,5 +1,25 @@
 package Arrays;
 
 public class LC724_Find_Pivot_Index {
-    
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+
+        for (int num : nums) {
+            total += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = total - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
 }
