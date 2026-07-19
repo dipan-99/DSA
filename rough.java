ae0753723e0a2b2bc34d5fd47f74ee973f210830
Dipan <dipansarkar76@gmail.com>
2026-07-19 23:26:21 +0530

    Remove unused prefix sum calculation and input handling from rough class

--- rough.java
+++ rough.java
@@ -2,35 +2,6 @@ import java.util.*;
 
 public class rough {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        int[] prefix = new int[n];
-        prefix[0] = arr[0];
-
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + arr[i];
-        }
-
-        int L = sc.nextInt();
-        int R = sc.nextInt();
-
-        System.out.println(rangeSum(prefix, L, R));
-        sc.close();
-    }
-
-    public static int rangeSum(int[] prefix, int L, int R) {
-        if (L == 0) {
-            return prefix[R];
-        }
-
-        return prefix[R] - prefix[L - 1];
+        
     }
 }
