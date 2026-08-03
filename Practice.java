c3ec17ea725fb1f57c7e8c78e31242d3812cf263
Dipan <dipansarkar76@gmail.com>
2026-08-03 20:32:20 +0530

    Refactor Practice class to improve prefix sum calculation and enhance input handling

--- Practice.java
+++ Practice.java
@@ -2,6 +2,37 @@ import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] prefix =new  int[n];
+        prefix[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+
+        int ans = PrefixSum(prefix, l, r);
+
+        System.out.println(ans);
+
+        sc.close();
+    }
+
+    public static int PrefixSum(int[] arr, int l, int r) {
+        if (l == 0) {
+            return arr[r];
+        }
+
+        return arr[r] - arr[l - 1];
     }
 }
