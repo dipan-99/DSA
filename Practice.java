451f6f49103994eddb8f8e159a331fdb947a6a6e
Dipan <dipansarkar76@gmail.com>
2026-08-04 15:32:34 +0530

    Refactor Practice class to improve code readability by removing unnecessary blank lines

--- Practice.java
+++ Practice.java
@@ -1,47 +1,42 @@
 import java.util.*;
 
 public class Practice {
-
     public static void main(String[] args) {
 
         // Input
-
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-
         int[] arr = new int[n];
 
         for (int i = 0; i < arr.length; i++) {
-
             arr[i] = sc.nextInt();
-
         }
 
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+
         // Answer
-
         int[] prefix = new int[n];
-
         prefix[0] = arr[0];
 
         for (int i = 1; i < n; i++) {
-
             prefix[i] = prefix[i - 1] + arr[i];
-
         }
 
         // Output
+        int ans = prefixSum(prefix, l, r);
 
-        for (int i = 0; i < n; i++) {
-
-            System.out.print(prefix[i] + " ");
-
-        }
-
-        System.out.println();
+        System.out.println(ans);
 
         sc.close();
-
     }
 
+    public static int prefixSum(int[] arr, int l, int r) {
+        if (l == 0) {
+            return arr[r];
+        }
+
+        return arr[r] - arr[l - 1];
+    }
 }
