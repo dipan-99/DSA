d08cbaac0182c3a20f1c26b6bb8a35bee8ecdd9d
Dipan <dipansarkar76@gmail.com>
2026-08-04 15:31:52 +0530

    Refactor Practice class to remove unused variables and streamline prefix sum calculation

--- Practice.java
+++ Practice.java
@@ -1,36 +1,47 @@
 import java.util.*;
 
 public class Practice {
+
     public static void main(String[] args) {
 
         // Input
+
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+
         int[] arr = new int[n];
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
 
-        int k = sc.nextInt();
+            arr[i] = sc.nextInt();
+
+        }
 
         // Answer
-        int sum = 0;
 
-        for (int i = 0; i < k; i++) {
-            sum += arr[i];
+        int[] prefix = new int[n];
+
+        prefix[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+
+            prefix[i] = prefix[i - 1] + arr[i];
+
         }
 
-        int maxSum = sum;
+        // Output
 
-        for (int i = k; i < n; i++) {
-            sum = sum - arr[i - k] + arr[i];
-            maxSum = Math.max(maxSum, sum);
+        for (int i = 0; i < n; i++) {
+
+            System.out.print(prefix[i] + " ");
+
         }
 
-        System.out.println(maxSum);
+        System.out.println();
 
         sc.close();
+
     }
+
 }
