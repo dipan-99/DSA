7bc1009e64b7dcd0ff851660bc45532e011ef2bb
Dipan <dipansarkar76@gmail.com>
2026-08-11 11:18:00 +0530

    Refactor Sliding_window class to enhance input handling and improve output formatting for sliding window sums

--- Sliding_Window/Basics/Sliding_window.java
+++ Sliding_Window/Basics/Sliding_window.java
@@ -1,5 +1,55 @@
 package Sliding_Window.Basics;
 
+import java.util.*;
+
 public class Sliding_window {
-    
+    public static void main(String[] args) {
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int k = 3;
+
+        // Output
+        System.out.println("Slinding Window sums");
+        slidingWindow(arr, k);
+
+        sc.close();
+    }
+
+    public static void slidingWindow(int[] arr, int k) {
+        int windowSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        // First Window
+        System.out.print(windowSum + " ");
+
+        // Remaining Window
+        for (int i = k; i < arr.length; i++) {
+            windowSum = windowSum - arr[i - k] + arr[i];
+
+            System.out.print(windowSum + " ");
+        }
+
+        System.out.println();
+    }
 }
+/*
+ * Sample Input
+ *
+ * 6
+ * 1 7 3 6 5 6
+ *
+ * Output
+ *
+ * 11 16 14 17
+ */
