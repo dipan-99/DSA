56c615e8bc14cbff9c8fd0d44a49c240965676c5
Dipan <dipansarkar76@gmail.com>
2026-08-31 18:02:42 +0530

    Implement findIntersectionValues method to return counts of common elements between two arrays

--- Hashing/LC2956_Find_Common_Elements_Between_Two_Arrays.java
+++ Hashing/LC2956_Find_Common_Elements_Between_Two_Arrays.java
@@ -1,8 +1,35 @@
 package Hashing;
 
+import java.util.HashSet;
+
 public class LC2956_Find_Common_Elements_Between_Two_Arrays {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        
-        return new int[] {};
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for (Integer x : nums1) {
+            set1.add(x);
+        }
+
+        for (Integer x : nums2) {
+            set2.add(x);
+        }
+
+        int count1 = 0;
+        int count2 = 0;
+
+        for (Integer x : nums1) {
+            if (set2.contains(x)) {
+                count1++;
+            }
+        }
+
+        for (Integer x : nums2) {
+            if (set1.contains(x)) {
+                count2++;
+            }
+        }
+
+        return new int[] {count1, count2};
     }
 }
