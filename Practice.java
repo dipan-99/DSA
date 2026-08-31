b13afb17b89a892c43f23db30524cca7ff5751d6
Dipan <dipansarkar76@gmail.com>
2026-08-31 21:36:50 +0530

    Refactor code structure for improved readability and maintainability

--- Practice.java
+++ Practice.java
@@ -2,34 +2,10 @@ import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
-        // Input
-        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> set = new HashSet<>();
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        int k = sc.nextInt();
-
-        // Answer
-        int sum = 0;
-
-        for (int i = 0; i < k; i++) {
-            sum += arr[i];
-        }
-
-        int minSum = sum;
-
-        for (int i = k; i < n; i++) {
-            sum = sum - arr[i - k] + arr[i];
-            minSum = Math.min(minSum, sum);
-        }
-
-        System.out.println(minSum);
-
-        sc.close();
+        System.out.println(set.add(10));
+        System.out.println(set.add(20));
+        System.out.println(set.add(10));
     }
 }
