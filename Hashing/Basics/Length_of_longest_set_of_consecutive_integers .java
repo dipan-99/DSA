e165ff32219312d6c4e85defe3e683fafe6349be
Dipan <dipansarkar76@gmail.com>
2026-09-02 18:35:26 +0530

    Add Length_of_longest_set_of_consecutive_integers class to find the length of the longest set of consecutive integers using HashSet

---
+++ Hashing/Basics/Length_of_longest_set_of_consecutive_integers .java
@@ -0,0 +1,33 @@
+package Hashing.Basics;
+
+public class Length_of_longest_set_of_consecutive_integers {
+    public int solve(int[] A) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        // Put all elements in HashSet
+        for (int num : A) {
+            set.add(num);
+        }
+
+        int maxLength = 0;
+
+        for (int num : set) {
+
+            if (!set.contains(num - 1)) {
+
+                int current = num;
+                int length = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    length++;
+                }
+
+                maxLength = Math.max(maxLength, length);
+            }
+        }
+
+        return maxLength;
+    }
+}
