bfd195743f293d12da5cb1e4ab095cf5eb291b00
Dipan <dipansarkar76@gmail.com>
2026-09-03 18:34:51 +0530

    Refactor Insertion_of_X_at_Kth_node class to improve structure and readability

--- Linked_List/Basics/Insertion_of_X_at_Kth_node.java
+++ Linked_List/Basics/Insertion_of_X_at_Kth_node.java
@@ -1,5 +1,33 @@
 package Linked_List.Basics;
 
 public class Insertion_of_X_at_Kth_node {
-    
+    public class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node insert(Node head, int x, int k) {
+        Node newNode = new Node(x);
+
+        if (k == 1) {
+            newNode.next = head;
+            return newNode;
+        }
+
+        Node current = head;
+
+        for (int i = 1; i < k - 1; i++) {
+            current = current.next;
+        }
+
+        newNode.next = current.next;
+        current.next = newNode;
+
+        return head;
+    }
 }
