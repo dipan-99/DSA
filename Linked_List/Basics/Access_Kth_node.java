75a2abb057526a6a5a2585402b7c5cc655087bff
Dipan <dipansarkar76@gmail.com>
2026-09-05 18:31:34 +0530

    Add Access_Kth_node class to implement access to the Kth node in a linked list

--- Linked_List/Basics/Access_Kth_node.java
+++ Linked_List/Basics/Access_Kth_node.java
@@ -1,5 +1,35 @@
 package Linked_List.Basics;
 
 public class Access_Kth_node {
+    public int solve(Node A, int B) {
+        Node curr = A;
 
+        if (B == 0) {
+            return A.data;
+        }
+
+        for (int i = 0; i < B; i++) {
+            if (curr == null) {
+                return -1;
+            }
+
+            curr = curr.next;
+        }
+
+        if (curr == null) {
+            return -1;
+        }
+
+        return curr.data;
+    }
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
