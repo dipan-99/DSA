2e90e4599bc6c5616bd4261e28d8cc0d4ec1f978
Dipan <dipansarkar76@gmail.com>
2026-09-05 17:41:14 +0530

    Refactor insert method in Insertion_of_X_at_Kth_node class for consistency and clarity

--- Linked_List/Basics/Insertion_of_X_at_Kth_node.java
+++ Linked_List/Basics/Insertion_of_X_at_Kth_node.java
@@ -14,19 +14,19 @@ public class Insertion_of_X_at_Kth_node {
     public Node insert(Node head, int x, int k) {
         Node newNode = new Node(x);
 
+        Node curr = head;
+
         if (k == 1) {
             newNode.next = head;
             return newNode;
         }
 
-        Node current = head;
-
         for (int i = 1; i < k - 1; i++) {
-            current = current.next;
+            curr = curr.next;
         }
 
-        newNode.next = current.next;
-        current.next = newNode;
+        newNode.next = curr.next;
+        curr.next = newNode;
 
         return head;
     }
