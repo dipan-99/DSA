3331b87a9ccd4fbaec2f0ca0e24ff4961a200095
Dipan <dipansarkar76@gmail.com>
2026-09-08 14:53:39 +0530

    Remove removeNthFromEnd method and related Node class; add LC1290_Convert_Binary_Number_in_a_Linked_List_to_Integer class

---
+++ Linked_List/LC1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -0,0 +1,5 @@
+package Linked_List;
+
+public class LC1290_Convert_Binary_Number_in_a_Linked_List_to_Integer {
+    
+}
--- Main.java
+++ Main.java
@@ -1,45 +1,9 @@
 public class Main {
-    public Node removeNthFromEnd(Node A, int B) {
-        Node curr = A;
+    public static void main(String[] args) {
+        String str = "";
 
-        int n = Len(A);
+        str += 2;
 
-        if (B >= n) {
-            return A.next;
-        }
-
-        int Bnew = n - B + 1;
-
-        for (int i = 0; i < Bnew - 2 && curr != null; i++) {
-            curr = curr.next;
-        }
-
-        if (curr != null && curr.next != null) {
-            curr.next = curr.next.next;
-        }
-
-        return A;
-    }
-
-    public static int Len(Node head) {
-        Node curr = head;
-        int count = 0;
-
-        while (curr != null) {
-            curr = curr.next;
-            count++;
-        }
-
-        return count;
-    }
-
-    class Node {
-        int data;
-        Node next;
-
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
+        System.out.println(Integer.parseInt(str));
     }
 }
