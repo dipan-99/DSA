3d149bd3e31655f40c648df2be654ac91721793a
Dipan <dipansarkar76@gmail.com>
2026-09-08 14:15:45 +0530

    Add removeNthFromEnd method to remove the N-th node from the end of a linked list and implement LC206_Reverse_Linked_List class to reverse a linked list

---
+++ Linked_List/LC206_Reverse_Linked_List.java
@@ -0,0 +1,35 @@
+package Linked_List;
+
+public class LC206_Reverse_Linked_List {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,45 @@
 public class Main {
-    public static void main(String[] args) {
+    public Node removeNthFromEnd(Node A, int B) {
+        Node curr = A;
 
+        int n = Len(A);
 
+        if (B >= n) {
+            return A.next;
+        }
 
+        int Bnew = n - B + 1;
+
+        for (int i = 0; i < Bnew - 2 && curr != null; i++) {
+            curr = curr.next;
+        }
+
+        if (curr != null && curr.next != null) {
+            curr.next = curr.next.next;
+        }
+
+        return A;
+    }
+
+    public static int Len(Node head) {
+        Node curr = head;
+        int count = 0;
+
+        while (curr != null) {
+            curr = curr.next;
+            count++;
+        }
+
+        return count;
+    }
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
 }
