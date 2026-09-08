cdb9331b45baed6cd9fae67edc260005152b3f06
Dipan <dipansarkar76@gmail.com>
2026-09-09 02:36:32 +0530

    Add Check_Odd_or_Even class to determine if a number is odd using bit manipulation

--- Bit_Manipulation/Basics/Check_Odd_or_Even.java
+++ Bit_Manipulation/Basics/Check_Odd_or_Even.java
@@ -1,6 +1,6 @@
 package Bit_Manipulation.Basics;
 
-public class Check_if_a_Number_is_Odd_or_Not {
+public class Check_Odd_or_Even {
     public boolean isOdd(int n) {
         if ((n & 1) == 0) {
             return false;
