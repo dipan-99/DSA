8e3e9cafb24fca8c757884f1598e5576a23c6e92
Dipan <dipansarkar76@gmail.com>
2026-09-09 03:01:52 +0530

    Add LC137_Single_Number_II class to implement a solution for finding the single number in an array using bit manipulation

---
+++ Bit_Manipulation/LC137_Single_Number_II.java
@@ -0,0 +1,21 @@
+package Bit_Manipulation;
+
+public class LC137_Single_Number_II {
+    
+
+    // public int singleNumber(int[] nums) {
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+
+    //     for (int x : nums) {
+    //         map.put(x, map.getOrDefault(x, 0) + 1);
+    //     }
+
+    //     for (int x : nums) {
+    //         if (map.get(x) == 1) {
+    //             return x;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+}
