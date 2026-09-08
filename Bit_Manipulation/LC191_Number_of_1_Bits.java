efced0b27ad61748d8fa9e0b2815a117d2db1511
Dipan <dipansarkar76@gmail.com>
2026-09-09 01:38:54 +0530

    Add LC191_Number_of_1_Bits class to calculate the number of 1 bits in an integer

---
+++ Bit_Manipulation/LC191_Number_of_1_Bits.java
@@ -0,0 +1,14 @@
+package Bit_Manipulation;
+
+public class LC191_Number_of_1_Bits {
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        while (n > 0) {
+            n = n & (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+}
