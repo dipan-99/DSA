12d0c134c0d052edfd07f10217c670734a40ce04
Dipan <dipansarkar76@gmail.com>
2026-09-09 16:04:14 +0530

    Refactor LC260_Single_Number_III class to improve method naming and enhance code clarity

--- Bit_Manipulation/LC260_Single_Number_III.java
+++ Bit_Manipulation/LC260_Single_Number_III.java
@@ -3,10 +3,33 @@ package Bit_Manipulation;
 import java.util.*;
 
 public class LC260_Single_Number_III {
+    //BETTER --- TC-O(N), SC-O(1)
+
+    public int[] singleNumberSOL1(int[] nums) {
+        int xor = 0;
+
+        for (int x : nums) {
+            xor ^= x;
+        }
+
+        int right = (xor & (xor - 1)) ^ xor;
+
+        int b1 = 0, b2 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] & right) == 0) {
+                b1 ^= nums[i];
+            } else {
+                b2 ^= nums[i];
+            }
+        }
+
+        return new int[] { b1, b2 };
+    }
 
     // BRUTE FORCE --- TC-O(N), SC-O(N)
 
-    public int[] singleNumber(int[] nums) {
+    public int[] singleNumberSOL2(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int x : nums) {
