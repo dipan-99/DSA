134d1427cee31262b780dea03da44d228e5f99fb
Dipan <dipansarkar76@gmail.com>
2026-09-10 13:46:59 +0530

    Refactor LC29_Divide_Two_Integers class to improve logic and enhance readability

--- Bit_Manipulation/LC29_Divide_Two_Integers.java
+++ Bit_Manipulation/LC29_Divide_Two_Integers.java
@@ -2,34 +2,40 @@ package Bit_Manipulation;
 
 public class LC29_Divide_Two_Integers {
     public int divide(int dividend, int divisor) {
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
-            return Integer.MAX_VALUE;
+        if (dividend == divisor)
+            return 1;
+
+        boolean sign = true;
+
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
+            sign = false;
         }
 
-        long a = Math.abs((long) dividend);
-        long b = Math.abs((long) divisor);
-
-        boolean negative = (dividend < 0) ^ (divisor < 0);
+        long n = Math.abs((long) dividend);
+        long d = Math.abs((long) divisor);
 
         long ans = 0;
 
-        while (a >= b) {
-            long temp = b;
-            long multiple = 1;
+        while (n >= d) {
+            int count = 0;
 
-            while (a >= (temp << 1)) {
-                temp <<= 1;
-                multiple <<= 1;
+            while (n >= (d << (count + 1))) {
+                count++;
             }
 
-            a -= temp;
-            ans += multiple;
+            ans += (1L << count);
+            n -= (d << count);
         }
 
-        if (negative) {
-            ans = -ans;
-        }
+        if (ans == (1L << 31) && sign)
+            return Integer.MAX_VALUE;
 
-        return (int) ans;
+        if (ans == (1L << 31) && !sign)
+            return Integer.MIN_VALUE;
+
+        if (sign)
+            return (int) ans;
+        else
+            return (int) -ans;
     }
 }
