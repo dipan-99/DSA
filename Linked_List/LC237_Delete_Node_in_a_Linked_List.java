3c4361f331c48ac0c72618ebb000b97c5db6df64
Dipan <dipansarkar76@gmail.com>
2026-09-16 17:44:12 +0530

    Refactor LC237_Delete_Node_in_a_Linked_List class to include ListNode definition and improve code structure

--- Linked_List/LC237_Delete_Node_in_a_Linked_List.java
+++ Linked_List/LC237_Delete_Node_in_a_Linked_List.java
@@ -1,5 +1,26 @@
 package Linked_List;
 
 public class LC237_Delete_Node_in_a_Linked_List {
-    
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
