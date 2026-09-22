907f928001c9edbc8f2a6098ab96eaa6c05d6b81
Dipan <dipansarkar76@gmail.com>
2026-09-23 01:17:38 +0530

    Add LC160_Intersection_of_Two_Linked_Lists class with getIntersectionNode method to find the intersection of two linked lists

---
+++ Linked_List/LC160_Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,26 @@
+package Linked_List;
+
+import Linked_List.LC148_Sort_List.ListNode;
+
+public class LC160_Intersection_of_Two_Linked_Lists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        while (p1 != p2) {
+            if (p1 == null) {
+                p1 = headB;
+            } else {
+                p1 = p1.next;
+            }
+
+            if (p2 == null) {
+                p2 = headA;
+            } else {
+                p2 = p2.next;
+            }
+        }
+
+        return p1;
+    }
+}
