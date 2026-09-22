5bf01672e4a5c1a5c94f53a404254cadca8216f5
Dipan <dipansarkar76@gmail.com>
2026-09-22 20:08:06 +0530

    Add LC148_Sort_List class with sortList and mergeTwoLists methods

---
+++ Linked_List/LC148_Sort_List.java
@@ -0,0 +1,67 @@
+package Linked_List;
+
+public class LC148_Sort_List {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode right = slow.next;
+        slow.next = null;
+
+        ListNode first = sortList(head);
+        ListNode second = sortList(right);
+
+        return mergeTwoLists(first, second);
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode(-1);
+        ListNode curr = temp;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (list1 == null) {
+            curr.next = list2;
+        } else {
+            curr.next = list1;
+        }
+
+        return temp.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
