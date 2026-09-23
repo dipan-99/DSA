caa3fe7bd38e197151fbe2c04313ba9d4efa4525
Dipan <dipansarkar76@gmail.com>
2026-09-23 15:26:02 +0530

    Refactor Traversal_in_DLL class to improve structure and readability

--- Linked_List/Basics_Doubly/Traversal_in_DLL.java
+++ Linked_List/Basics_Doubly/Traversal_in_DLL.java
@@ -1,5 +1,46 @@
 package Linked_List.Basics_Doubly;
 
 public class Traversal_in_DLL {
-    
+    static class ListNode {
+        int data;
+        ListNode prev;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ListNode first = new ListNode(10);
+        ListNode second = new ListNode(20);
+        ListNode third = new ListNode(30);
+
+        first.next = second;
+        second.prev = first;
+
+        second.next = third;
+        third.prev = second;
+
+        // forward traversal
+        ListNode head = first;
+        ListNode curr = head;
+
+        while (curr != null) {
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
+
+        // backward traversal
+        ListNode tail = third;
+        ListNode cur = tail;
+
+        while (cur != null) {
+            System.out.println(cur.data);
+            cur = cur.prev;
+        }
+    }
 }
