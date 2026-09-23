ddeb70a84b85dec19cdfd8fe2b698488ce12122c
Dipan <dipansarkar76@gmail.com>
2026-09-23 15:49:50 +0530

    Add Insertion_in_DLL class for inserting nodes in a doubly linked list

---
+++ Linked_List/Basics_Doubly/Insertion_in_DLL.java
@@ -0,0 +1,49 @@
+package Linked_List.Basics_Doubly;
+
+public class Insertion_in_DLL {
+    static class ListNode {
+        int data;
+        ListNode prev;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    static int x = 0;
+
+    //insert at the start
+    public static void insertStart(ListNode head) {
+        ListNode newNode = new ListNode(x);
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    //insert at the end
+    public static void insertEnd(ListNode tail) {
+        ListNode newNode = new ListNode(x);
+
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
+    //insert at a position
+    public static void insertPos(ListNode head) {
+        ListNode newNode = new ListNode(x);
+        ListNode curr = head;
+
+        // first travel to the previous node of the required position then
+
+        newNode.prev = curr;
+        newNode.next = curr.next;
+
+        curr.next.prev = newNode;
+        curr.next = newNode;
+    }
+}
