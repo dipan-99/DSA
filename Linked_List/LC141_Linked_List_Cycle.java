e43208688fed8a9ad80ae25ac35ca8bfe7c4eaed
Dipan <dipansarkar76@gmail.com>
2026-09-24 15:29:30 +0530

    Add LC141_Linked_List_Cycle class to detect cycles in a linked list

---
+++ Linked_List/LC141_Linked_List_Cycle.java
@@ -0,0 +1,29 @@
+package Linked_List;
+
+public class LC141_Linked_List_Cycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
