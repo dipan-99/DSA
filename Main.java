b7be03870b062ba8b6acca5026945f125714f67e
Dipan <dipansarkar76@gmail.com>
2026-09-25 16:00:19 +0530

    Add BankAccount class and implement Car class with display method

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
 public class Main {
+    class BankAccount {
+        String accountHolder;
+        String accountNumber;
+        double balance;
+
+        void deposit() {
+
+        }
+
+        void withdraw() {
+
+        }
+
+        void displayBalance() {
+
+        }
+    }
     public static void main(String[] args) {
-        
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+        c1.brand = "BMW";
+        c1.model = "M4";
+        c1.price = 8500000;
+
+        c2.brand = "Audi";
+        c2.model = "R8";
+        c2.price = 20000000;
     }
 }
---
+++ OOPS/oops1.java
@@ -0,0 +1,33 @@
+package OOPS;
+
+public class oops1 {
+    static class Car {
+        String brand;
+        String model;
+        int price;
+
+        void display() {
+            System.out.println("Brand: " + brand);
+            System.out.println("Model: " + model);
+            System.out.println("Price: " + price);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+        c1.brand = "BMW";
+        c1.model = "M4";
+        c1.price = 8500000;
+
+        c2.brand = "Audi";
+        c2.model = "R8";
+        c2.price = 20000000;
+
+        c1.display();
+        System.out.println();
+        c2.display();
+    }
+}
