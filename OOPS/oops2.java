ae90d9a190112f24d363386f698369cef6d13e99
Dipan <dipansarkar76@gmail.com>
2026-09-25 16:10:59 +0530

    Add BankAccount class with deposit, withdraw, and displayBalance methods in oops2.java

---
+++ OOPS/oops2.java
@@ -0,0 +1,33 @@
+package OOPS;
+
+public class oops2 {
+    static class BankAccount {
+        String accountHolder;
+        long accountNumber;
+        double balance;
+
+        void deposit(double amount) {
+            balance += amount;
+        }
+
+        void withdraw(double amount) {
+            balance -= amount;
+        }
+
+        void displayBalance() {
+            System.out.println("Balance: " + balance);
+        }
+    }
+
+    public static void main(String[] args) {
+        BankAccount b1 = new BankAccount();
+
+        b1.accountHolder = "Dipan";
+        b1.accountNumber = 123456789;
+        b1.balance = 10000;
+
+        b1.deposit(5000);
+        b1.withdraw(3000);
+        b1.displayBalance();
+    }
+}
