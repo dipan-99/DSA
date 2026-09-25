9d543f0a1b66fbc7ea77fabadaa2f2de631ae28c
Dipan <dipansarkar76@gmail.com>
2026-09-26 01:07:04 +0530

    Add oops8.java with BankAccount class to manage account operations and display balance

---
+++ OOPS/Basics/oops8.java
@@ -0,0 +1,42 @@
+package OOPS.Basics;
+
+public class oops8 {
+    static class BankAccount {
+        private String accountHolder;
+        private double balance;
+
+        BankAccount(String accountHolder, double balance) {
+            this.accountHolder = accountHolder;
+            this.balance = balance;
+        }
+
+        void deposit(double amount) {
+            if (amount <= 0) {
+                return;
+            }
+
+            balance += amount;
+        }
+
+        void withdraw(double amount) {
+            if (amount <= 0 || amount > balance) {
+                return;
+            }
+
+            balance -= amount;
+        }
+
+        void displayBalance() {
+            System.out.println("Balance: " + balance);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        BankAccount b1 = new BankAccount("Dipan", 10000);
+
+        b1.deposit(5000);
+        b1.withdraw(3000);
+        b1.displayBalance();
+    }
+}
